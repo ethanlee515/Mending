c2834defd73584c472fdd20c8bfe19b9347af84b
Yi Lee <ylee1228@umd.edu>
2025-07-14 08:35:49 +0100

    Start working on inference rules

--- theories/Logic/RhlAe.v
+++ theories/Logic/RhlAe.v
@@ -2,6 +2,7 @@ From Stdlib Require Import Unicode.Utf8.
 Set Warnings "-ambiguous-paths,-notation-overridden,-notation-incompatible-format".
 From mathcomp Require Import all_ssreflect all_algebra.
 From mathcomp Require Import reals distr.
+From mathcomp Require Import realseq realsum exp.
 Set Warnings "ambiguous-paths,notation-overridden,notation-incompatible-format".
 From SSProve.Relational Require Import OrderEnrichedCategory.
 From SSProve.Crypt Require Import ChoiceAsOrd Couplings StateTransformingLaxMorph.
@@ -20,3 +21,25 @@ Definition equiv_with_additive_error
     let out1 := thetaFstd A1 c1 s1 in
     let out2 := thetaFstd A2 c2 s2 in
     ∃ d, coupling d out1 out2 ∧ \P_[ d ] post >= 1 - ε.
+
+(* I'm sure this belongs somewhere else
+ * and requires a bunch of other lemma *)
+Definition statistical_distance {T : choiceType} (P Q : {distr T / R}) : R :=
+  sum (fun x => `| P x - Q x |).
+
+(* There is probably a stronger version of this...? *)
+Lemma up_to_bad (out_t: ord_choiceType)
+  {mem_t: choiceType}
+  (c1 : @FrStP mem_t out_t) (c2 : @FrStP mem_t out_t)
+  (pre : pred (mem_t * mem_t))
+  (post : pred ((out_t * mem_t) * (out_t * mem_t)))
+  (ε : R) :
+  equiv_with_additive_error out_t out_t c1 c2
+    (fun mems => match mems with (mL, mR) => mL == mR end)
+    (fun results => match results with (resL, resR) => resL == resR end)
+    ε ->
+  forall s,
+    let out1 := thetaFstd out_t c1 s in
+    let out2 := thetaFstd out_t c2 s in
+    statistical_distance out1 out2 < ε.
+Admitted.
--- theories/Logic/RhlPythErrors.v
+++ theories/Logic/RhlPythErrors.v
@@ -1,5 +1,6 @@
 (* RHL with Pythagorean Errors *)
 
+From Stdlib Require Import Unicode.Utf8.
 From Stdlib Require Import List.
 Set Warnings "-ambiguous-paths,-notation-overridden,-notation-incompatible-format".
 From mathcomp Require Import all_ssreflect all_algebra.
@@ -11,6 +12,7 @@ From SSProve.Crypt Require Import Axioms StateTransfThetaDens.
 From SSProve Require Import FreeProbProg.
 From SSProve.Crypt Require Import choice_type SubDistr.
 From VerifiedCKKS Require Import KL.
+From VerifiedCKKS Require Import RhlAe.
 
 Import ListNotations.
 Local Open Scope list_scope.
@@ -21,20 +23,37 @@ Arguments bindrFree {_ _ _ _} _ _.
 Arguments callrFree {_ _ } _.
 
 Inductive equiv_with_pythagorean_errors :
-  forall (out_t : ord_choiceType) (mem_t : choiceType),
+  ∀ (out_t : ord_choiceType) (mem_t : choiceType),
     @FrStP mem_t out_t -> @FrStP mem_t out_t -> list R -> Prop :=
 | pyth_ret_eq {out_t : ord_choiceType} (mem_t : choiceType) (v : out_t) :
     equiv_with_pythagorean_errors out_t mem_t (retrFree v) (retrFree v) []
 | pyth_eps_samp {supp_t : choice_type} (mem_t : choiceType)
-    (P Q : {distr supp_t / R}) (eps : R) :
-  δ_KL P Q < eps ->
+    (P Q : {distr supp_t / R}) (ε : R) :
+  δ_KL P Q < ε ->
   equiv_with_pythagorean_errors _ mem_t
     (callrFree (samplee (existT _ supp_t P)))
-    (callrFree (samplee (existT _ supp_t Q))) [ eps ]
+    (callrFree (samplee (existT _ supp_t Q))) [ ε ]
 | pyth_bind_eq (mem_t: choiceType) (outA_t outB_t : ord_choiceType)
     (pL pR : @FrStP mem_t outA_t) (qL qR : outA_t -> @FrStP mem_t outB_t)
     (lst1 lst2 : list R) :
-  equiv_with_pythagorean_errors outA_t mem_t pL pR lst1 ->
-  (forall a, equiv_with_pythagorean_errors outB_t mem_t (qL a) (qR a) lst2) ->
+  equiv_with_pythagorean_errors outA_t mem_t pL pR lst1 →
+  (∀ a, equiv_with_pythagorean_errors outB_t mem_t (qL a) (qR a) lst2) →
   equiv_with_pythagorean_errors outB_t mem_t
     (bindrFree pL qL) (bindrFree pR qR) (lst1 ++ lst2).
+
+Fixpoint sum_squares (lst : list R) :=
+  match lst with
+  | [] => 0
+  | head :: tail => head * head + sum_squares tail
+  end.
+
+Definition l2_norm (eps_lst : list R) := Num.sqrt (sum_squares eps_lst).
+
+Theorem mw18 (out_t : ord_choiceType) (mem_t : choiceType)
+  (progL progR : @FrStP mem_t out_t) (eps_lst : list R) :
+  equiv_with_pythagorean_errors out_t mem_t progL progR eps_lst →
+  equiv_with_additive_error out_t out_t progL progR
+  (fun mems => match mems with (mL, mR) => mL == mR end)
+  (fun results => match results with (resL, resR) => resL == resR end)
+  (l2_norm eps_lst).
+Admitted.
