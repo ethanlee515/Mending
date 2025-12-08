6887b8ecc49747f429a9d00ec81a7ca628c02453
Yi Lee <ylee1228@umd.edu>
2025-12-08 01:12:47 +0000

    Fix Pyth judgment

--- theories/ProgramLogics/RhlPythErrors.v
+++ theories/ProgramLogics/RhlPythErrors.v
@@ -2,10 +2,11 @@
 
 From Stdlib Require Import Unicode.Utf8.
 From Stdlib Require Import List.
-(* Require Import Stdlib.Vectors.Vector. *)
+From Stdlib Require Import Lia.
 Set Warnings "-ambiguous-paths,-notation-overridden,-notation-incompatible-format".
 From mathcomp Require Import all_ssreflect all_algebra.
 From mathcomp Require Import reals distr.
+From mathcomp Require Import lra.
 Set Warnings "ambiguous-paths,notation-overridden,notation-incompatible-format".
 From SSProve.Relational Require Import OrderEnrichedCategory.
 From SSProve.Crypt Require Import ChoiceAsOrd Couplings StateTransformingLaxMorph.
@@ -16,6 +17,8 @@ From SSProve Require Import pkg_core_definition pkg_advantage.
 From Mending Require Import KL.
 From Mending Require Import RhlAe.
 From Mending Require Import DistrExtras.
+From Mending Require Import RhlAe.
+Local Open Scope UtbNotations.
 
 Import ListNotations.
 Import pkg_heap.
@@ -34,8 +37,6 @@ Fixpoint sum_squares (lst : list R) :=
 
 Definition l2_norm (eps_lst : list R) := Num.sqrt (sum_squares eps_lst).
 
-From Stdlib Require Import Lia.
-From mathcomp Require Import lra.
 
 Program Definition lift_i {ℓ : nat} {t : 'I_(ℓ.+1)} (i : 'I_t) : 'I_(ℓ.+1) :=
   widen_ord _ i.
@@ -48,8 +49,6 @@ Defined.
 Definition pythJudgment
   {ℓ : nat}
   {outs_t : 'I_ℓ.+1 -> choiceType}
-  {prod_out_t : choiceType}
-  {marginal : forall (i : 'I_ℓ.+1), prod_out_t -> (outs_t i * heap_choiceType)}
   {inL_t inR_t : ord_choiceType}
   (progL : inL_t -> raw_code (outs_t ord_max))
   (progR : inR_t -> raw_code (outs_t ord_max))
@@ -57,16 +56,19 @@ Definition pythJudgment
   (post : pred (outs_t ord_max * heap_choiceType))
   (s : (ℓ.+1).-tuple R) :=
   ∀ memL memR xL xR, pre ((xL, memL), (xR, memR)) →
-  exists (P Q : {distr prod_out_t / R}),
+  exists
+  (prod_out_t : choiceType)
+  (marginal : forall (i : 'I_ℓ.+1), prod_out_t -> (outs_t i * heap_choiceType))
+  (P Q : {distr prod_out_t / R}),
   let outL := Pr_code (progL xL) memL in
   let outR := Pr_code (progR xR) memR in
   (* Marginals match *)
   dmargin (marginal ord_max) P =1 outL /\
   dmargin (marginal ord_max) Q =1 outR /\
   (* KL closeness *)
-  (forall (t : 'I_ℓ.+1) (a : forall (i : 'I_t), outs_t (lift_i i)),
-  let Pa := P in
-  let Qa := Q in
+  (forall (t : 'I_ℓ.+1) (a : forall (i : 'I_t), outs_t (lift_i i) * heap_choiceType),
+  let Pa := dcond P (fun ys => [forall i : 'I_t, a i == marginal (lift_i i) ys]) in
+  let Qa := dcond Q (fun ys => [forall i : 'I_t, a i == marginal (lift_i i) ys]) in
   δ_KL (dmargin (marginal t) Pa) (dmargin (marginal t) Qa) < tnth s t) /\
   (* post-condition satisfied *)
   forall x, x \in dinsupp outL -> post x.
@@ -76,6 +78,28 @@ Local Open Scope pyth_scope.
 
 Notation "⊨Pyth ⦃ pre ⦄ progL ≈( s ) progR ⦃ post ⦄" :=
   (pythJudgment progL progR pre post s) : pyth_scope.
+
+Definition two_norm {n : nat} (s : (n.+1).-tuple R) : R.
+Admitted.
+
+
+Lemma MicciancioWalterRule
+  {ℓ : nat}
+  {inL_t inR_t out_t : ord_choiceType}
+  (progL : inL_t -> raw_code out_t)
+  (progR : inR_t -> raw_code out_t)
+  (pre : pred ((inL_t * heap_choiceType) * (inR_t * heap_choiceType)))
+  (post : pred (out_t * heap_choiceType))
+  (s : (ℓ.+1).-tuple R) :
+  ⊨Pyth ⦃ pre ⦄ progL ≈( s ) progR ⦃ post ⦄ ->
+  let delta := two_norm s in
+  ⊨UTB ⦃ pre ⦄ progL ≈( delta ) progR ⦃
+    fun outs =>
+      let '((y_1, m_1'), (y_2, m_2')) := outs in
+      post (y_1, m_1') && (y_1 == y_2) && (m_1' == m_2')⦄.
+Proof.
+move => *.
+Admitted.
 
 (**
     @FrStP mem_t out_t → @FrStP mem_t out_t → list R → Prop :=
